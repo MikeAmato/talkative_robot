56c6985bdc7e70203171acc10bce9df6dd312b56
blackmist87 <mike.blackmist@aemail4u.com>
2014-09-30 11:54:28 -0400

    Placed the contents into the new files

--- app/lib/talkative_robot.rb
+++ app/lib/talkative_robot.rb
@@ -1,37 +1,20 @@
 require 'pry'
 require 'CSV'
-
-
-
-
-
-
-
-#////////////////////////////////////////Methods/Functions above this line
-#people = [{name: "Michael Amato", age: 27, gender: "M"}, {name: "Joe User", age: 20, gender: "M"}]
-#author = {name: "Michael Amato", age: 27, gender: "M"}
-#user = {name: "Joe User", age: 20, gender: "M"}
-#puts author.reject{ |key, value| value != "Michael Amato" }
-#puts user.reject{ |key, value| value != "Michael Amato" }
-
-#grocery_list = read_from_csv('grocery list csv.csv')
-#print_csv(grocery_list)
-#write_to_csv(grocery_list, 'grocery list CSV.csv')
-
-#me = select_by_name(people, author[:name])
-#puts me
-#people_enum
+require '.\person'
+require '.\user'
+require '.\author'
+require '.\grocery list'
 
 user_info = User.get_user_info
 @user = User.new(user_info)
-
-
 @user.relative_to_75
 @user.car_rental
 @user.young_gender_message
 @user.age_and_gender_based_message
 @user.activity
 @user.first_initial_check
+
+
 
 col_data = []
 
